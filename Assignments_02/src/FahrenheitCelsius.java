3b4c58fe8ff8490ad3710610d8104a8d0fefb411
PatrickJaloScott <patrickjalo.scott@gmail.com>
2023-08-23 12:36:50 +0300

    Assignments 1.2 alustettu
    Aloitettu tehtäväsarja 1.2
    Eka tehtävä fahrenheit_celsius

---
+++ Assignments_02/src/FahrenheitCelsius.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class FahrenheitCelsius {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter temperature in Fahrenheit: ");
+        double f = input.nextDouble();
+        double c = (f - 32)/1.8;
+        System.out.println(f + " in Celsius is " + c);
+        input.close();
+    }
+}
