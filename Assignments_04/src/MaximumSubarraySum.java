7de053e08f2e2073780234470e5ef08514b02485
PatrickJaloScott <patrickjalo.scott@gmail.com>
2023-08-29 13:54:07 +0300

    Assignments 1.4 (2/3)
    Tehtäväsarjasta 1.4 tehtävää "Maximum Subarray Sum" tehty

---
+++ Assignments_04/src/MaximumSubarraySum.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+public class MaximumSubarraySum {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("How many numbers in the array?: ");
+        int arraySize = input.nextInt();
+        int[] array = new int[arraySize];
+        System.out.println("Provide the numbers for the array");
+        for (int i = 0; i < arraySize; i++) {
+            System.out.print("Enter number " + (i+1) + ": ");
+            array[i] = input.nextInt();
+        }
+        // variables for end result
+        int maxSum = 0, rangeMin = 0, rangeMax = 0;
+        // loop through the array
+        for (int i = 0; i < arraySize; i++) {
+            // loop between the main index i and the last number with subindex j
+            // i.e. [i-arraySize] or (i < j < arraySize)
+            for (int j = i; j < arraySize; j++) {
+                //System.out.print("Subarray: ");
+                // initiate variable for sum of the subarray
+                int subSum = 0;
+                // loop through the subarray with index k
+                // i.e. [i-j] or (i < k < J)
+                for (int k = i; k <= j; k++) {
+                    //System.out.print(array[k] + " ");
+                    // add up the sum of the subarray
+                    subSum += array[k];
+                }
+                //System.out.println("\tSum: " + subSum);
+                // check if the sum of the current subarray will be the new maximum sum
+                if(subSum > maxSum) {
+                    // set the new maximum sum
+                    maxSum = subSum;
+                    // set the range of where in the array the sum came from
+                    rangeMin = i+1;
+                    rangeMax = j+1;
+                }
+            }
+        }
+        System.out.println("\nMaximum sum: " + maxSum);
+        System.out.printf("Range: %d-%d", rangeMin, rangeMax);
+
+        input.close();
+    }
+}
