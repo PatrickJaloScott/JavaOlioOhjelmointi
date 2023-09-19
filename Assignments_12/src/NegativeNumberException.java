eb7fc5f3e22f20b6519a59903ccda37c221a6714
PatrickJaloScott <patrickjalo.scott@gmail.com>
2023-09-19 11:07:46 +0300

    Added exception handling
    Added exception NegativeNumberException for the sum operations

---
+++ Assignments_12/src/NegativeNumberException.java
@@ -0,0 +1,8 @@
+/**
+ * Exception class for negative number validation
+ */
+public class NegativeNumberException extends Exception {
+    public NegativeNumberException(String message) {
+        super(message);
+    }
+}
