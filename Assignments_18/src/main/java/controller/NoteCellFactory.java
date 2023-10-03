4073c70ae915405df1b0ec69f55f03706a566e88
PatrickJaloScott <patrickjalo.scott@gmail.com>
2023-10-03 13:16:09 +0300

    Assignments 6.4
    Tehtävä tehty, paitsi 5 ja 7 kohdat

---
+++ Assignments_18/src/main/java/controller/NoteCellFactory.java
@@ -0,0 +1,23 @@
+package controller;
+
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.util.Callback;
+import model.Note;
+
+public class NoteCellFactory implements Callback<ListView<Note>, ListCell<Note>> {
+    @Override
+    public ListCell<Note> call(ListView<Note> param) {
+        return new ListCell<>() {
+            @Override
+            protected void updateItem(Note item, boolean empty) {
+                super.updateItem(item, empty);
+                if(empty || item == null) {
+                    setText(null);
+                } else {
+                    setText(item.getNoteTitle());
+                }
+            }
+        };
+    }
+}
